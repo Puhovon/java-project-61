3746273417cdb3a296f00491fd34a1c3e15240c1
Puhovon <pv487732@gmail.com>
2024-02-01 01:01:04 +0300

    add getByName and linter

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,4 +1,12 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class Cli {
+    public static void getByName() {
+        System.out.println("Welcome to the Brain Games!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        System.out.println("Hello, " + sc.nextLine() + "!");
+    }
 }
