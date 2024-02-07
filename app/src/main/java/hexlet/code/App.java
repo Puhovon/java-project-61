cb0ce89ef7ae9c24d6600f8b95125e34399efbc3
Puhovon <pv487732@gmail.com>
2024-02-07 18:22:33 +0300

    Create GDC game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 import hexlet.code.games.Calculate;
 import hexlet.code.games.Even;
+import hexlet.code.games.Node;
 
 public class App {
 
@@ -10,10 +11,12 @@ public class App {
         final String greet = "1";
         final String even = "2";
         final String calculator = "3";
+        final String gdc = "4";
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calculate");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         System.out.print("Your choise: ");
         Scanner console = new Scanner(System.in);
@@ -28,6 +31,8 @@ public class App {
             case calculator:
                 Calculate.calculate();
                 break;
+            case gdc:
+                Node.node();
             default:
                 break;
         }
